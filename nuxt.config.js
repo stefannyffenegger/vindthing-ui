7ee9321f2e0b1c444f0a1a412f2eddf45c6bebcb
Benjamin Steffen <benjamin.steffen@students.ffhs.ch>
2020-10-14 23:09:00 +0200

    Initial Upload

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'VindThingUI',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL: 'http://localhost:8080/'
+  },
+
+  auth: {
+    rewriteRedirects: true,
+    redirect: {
+      login: false,
+      logout: '/',
+      callback: '/',
+      home: '/'
+    },
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/api/auth/signin', method: 'post', propertyName: 'accessToken' },
+          user: false,
+          logout: false
+        }
+      }
+    }
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
