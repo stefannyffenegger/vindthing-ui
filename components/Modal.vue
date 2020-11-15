1fe23c72167c5be030b623ba9d3668c37da676b7
Benjamin Steffen <benjamin.steffen@students.ffhs.ch>
2020-11-15 22:41:05 +0100

    store view refresh, added modal for creating stores, added card view

---
+++ components/Modal.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container m-5">
+    <h2 class="title">Stores</h2>
+    <div class="content">
+      <form method="post" @submit.prevent="createStore">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input
+              type="text"
+              class="input"
+              name="name"
+              v-model="form.name"
+              required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Description</label>
+          <div class="control">
+            <input
+              type="text"
+              class="input"
+              name="description"
+              v-model="form.description"
+              required
+            />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Location</label>
+          <div class="control">
+            <input
+              type="text"
+              class="input"
+              name="location"
+              v-model="form.location"
+              required
+            />
+          </div>
+        </div>
+        <div class="columns">
+          <div class="control column">
+            <button
+              @click.prevent="$parent.close()"
+              class="button is-dark is-fullwidth"
+            >
+              Close
+            </button>
+          </div>
+          <div class="control column">
+            <button type="submit" class="button is-dark is-fullwidth"
+            @click="$parent.close()">
+              Create Store
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from "vuex";
+import ModalForm from "~/components/Modal";
+
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        description: "",
+        location: "",
+      },
+    };
+  },
+
+  methods: {
+    async createStore() {
+      console.log(this.form);
+      this.$store.dispatch("stores/createStore", this.form);
+      this.form = [];
+    },
+  },
+};
+</script>
+
+
+<style lang="css" scoped>
+.modal {
+  background-color: rgba(0, 0, 0, 0.7);
+}
+</style>
--- pages/stores.vue
+++ pages/stores.vue
@@ -1,44 +1,41 @@
 <template>
   <section class="section">
-    <div class="container">
-      <h2 class="title">Stores</h2>
-      <div class="content">
-        <form method="post" @submit.prevent="createStore">
-          <div class="field">
-            <label class="label">Name</label>
-            <div class="control">
-              <input type="text" class="input" name="name" v-model="form.name" required />
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Description</label>
-            <div class="control">
-              <input
-                type="text"
-                class="input"
-                name="description"
-                v-model="form.description"
-                required
-              />
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Location</label>
-            <div class="control">
-              <input type="text" class="input" name="location" v-model="form.location" required />
-            </div>
-          </div>
-          <div class="control">
-            <button type="submit" class="button is-dark is-fullwidth">Create Store</button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <h2 class="title">Stores</h2>
+
+    <button
+      class="button is-dark is-fullwidth"
+      @click="isComponentModalActive = true"
+    >
+      New Store
+    </button>
+
+    <b-modal v-model="isComponentModalActive">
+      <LazyModal></LazyModal>
+    </b-modal>
 
     <hr />
 
-    <b-table :data="getAllStores">
-      <template v-if="column.searchable && !column.numeric" slot="searchable" slot-scope="props">
+    <div class="control">
+      <b-switch v-model="isCardLayout"
+        >Card View</b-switch
+      >
+    </div>
+
+    <b-table
+      :data="getAllStores"
+      :card-layout="isCardLayout"
+      :mobile-cards="isCardLayout"
+      hoverable
+      checkable
+      paginated
+      per-page="10"
+      sort-multiple
+    >
+      <template
+        v-if="column.searchable && !column.numeric"
+        slot="searchable"
+        slot-scope="props"
+      >
         <b-input
           v-model="props.filters[props.column.field]"
           placeholder="Search..."
@@ -47,28 +44,29 @@
         />
       </template>
 
-      <b-table-column field="id" label="ID" width="40" numeric v-slot="props">{{ props.row.id }}</b-table-column>
+      <b-table-column field="id" label="ID" width="40" numeric v-slot="props">{{
+        props.row.id
+      }}</b-table-column>
 
-      <b-table-column
-        searchable
-        field="name"
-        label="Name"
-        v-slot="props"
-      >{{ props.row.name }}</b-table-column>
+      <b-table-column searchable field="name" label="Name" v-slot="props">{{
+        props.row.name
+      }}</b-table-column>
 
       <b-table-column
         searchable
         field="description"
         label="Description"
         v-slot="props"
-      >{{ props.row.description }}</b-table-column>
+        >{{ props.row.description }}</b-table-column
+      >
 
       <b-table-column
         searchable
         field="location"
         label="Location"
         v-slot="props"
-      >{{ props.row.location }}</b-table-column>
+        >{{ props.row.location }}</b-table-column
+      >
 
       <b-table-column label="Delete" v-slot="props">
         <b-button @click="deleteStore(props.row.id)">Delete</b-button>
@@ -79,36 +77,32 @@
 
 <script>
 import { mapMutations, mapGetters } from "vuex";
+import ModalForm from "~/components/Modal";
 
 export default {
   data() {
     return {
-      form: {
-        name: "",
-        description: "",
-        location: ""
-      }
+      isComponentModalActive: false,
+      isCardLayout: false
     };
   },
 
   middleware: "auth",
   computed: {},
   computed: mapGetters({
-    getAllStores: "stores/getStores"
+    getAllStores: "stores/getStores",
   }),
   methods: {
     async deleteStore(store_id) {
       this.$store.dispatch("stores/deleteStore", store_id);
     },
-    async createStore() {
-      console.log(this.form);
-      this.$store.dispatch("stores/createStore", this.form);
-      this.form = [];
-    }
+    openModal() {
+      this.modalOpen = !this.modalOpen;
+    },
   },
 
   mounted() {
     this.$store.dispatch("stores/getStores");
-  }
+  },
 };
 </script>
