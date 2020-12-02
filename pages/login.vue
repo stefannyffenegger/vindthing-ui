04c6302a846c5136f64e94dc638fdb0db13db973
Stefan Nyffenegger <nyfi777@gmail.com>
2020-12-02 18:44:19 +0100

    -websocket poc

--- pages/login.vue
+++ pages/login.vue
@@ -8,34 +8,35 @@
           <Notification :message="error" v-if="error"/>
 
           <form method="post" @submit.prevent="login">
-            <div class="field">
-              <label class="label">Email</label>
-              <div class="control">
-                <input
-                  type="text"
-                  class="input"
-                  name="username"
-                  v-model="email"
-                />
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Password</label>
-              <div class="control">
-                <input
-                  type="password"
-                  class="input"
-                  name="password"
-                  v-model="password"
-                />
-              </div>
-            </div>
+
+            <b-field label="E-Mail">
+              <b-input
+                type="email"
+                placeholder="Email"
+                icon="email"
+                name="username"
+                v-model="email"
+              ></b-input>
+            </b-field>
+            <b-field label="Password">
+              <b-input
+                type="password"
+                placeholder="Password"
+                password-reveal
+                icon="key"
+                name="password"
+                v-model="password"
+              ></b-input>
+            </b-field>
+
             <div class="control">
               <button type="submit" class="button is-dark is-fullwidth">Log In</button>
             </div>
           </form>
           <div class="has-text-centered" style="margin-top: 20px">
-            <p>Don't have an account? <nuxt-link to="/register">Register</nuxt-link></p>
+            <p>Don't have an account?
+              <nuxt-link to="/register">Register</nuxt-link>
+            </p>
           </div>
         </div>
       </div>
@@ -65,8 +66,8 @@ export default {
       try {
         await this.$auth.loginWith('local', {
           data: {
-          email: this.email,
-          password: this.password
+            email: this.email,
+            password: this.password
           }
         })
 
