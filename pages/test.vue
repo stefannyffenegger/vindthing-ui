3c2f3f2edc476fb3e1d570e8c71fd335bcd4e8fc
Benjamin Steffen <benjamin.steffen@students.ffhs.ch>
2020-12-24 15:06:29 +0100

    docker update

---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <h2 class="title has-text-centered">Welcome back!</h2>
+          <p>{{$config}}</p>
+          <p>{{$config.BASE_URL_API}}</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  /* Guest Component to let anonymous Users on this page */
+  middleware: "guest",
+
+};
+</script>
